b7af58b85012f192e33b10c9c33833809e6e8085
Mete Durlu <meted@linux.ibm.com>
2024-01-06 13:34:00 +0100

    submitting progress #1

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -3,13 +3,15 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    let mut x: u8;
+    
+    x = 5;
     if x == 10 {
         println!("x is ten!");
+        x += 1;
+        println!("x +1 is {}!", x);
     } else {
-        println!("x is not ten!");
+        println!("x is not ten! {}", x);
     }
 }
